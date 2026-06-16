d496fdedce27f7fe9aad154acb21ab5851fa906c
shovitgoyal551 <shovitgoyal551@gmail.com>
2026-06-16 17:03:50 +0530

    Create simple calculator.py
    this is a simple calculator
    calculate sum subtract muliply divide

---
+++ simple calculator.py
@@ -0,0 +1,11 @@
+a=int(input("enter a value="))
+b=int(input("enter b value="))
+
+#addition
+print("addition is",a+b)
+#subtraction
+print("subtraction is",a-b)
+#multipliction
+print("multiply is",a*b)
+#divition
+print("division is",a/b)
