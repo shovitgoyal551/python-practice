dc21fd6738005504925e4030776eb612ac85ea97
shovit goyal.py <shovitgoyal551@gmail.com>
2026-06-24 15:54:27 +0530

    check conditions

---
+++ conditional statement.py
@@ -0,0 +1,19 @@
+
+
+light = "brown"
+
+if(light=="red"):
+    print("stop")
+elif(light=="yellow"):
+    print("go slow")
+elif(light=="green"):
+    print("go")
+else:
+    print("not found")
+
+age = 21
+
+if(age<=18):
+    print("can vote")
+else:
+    print("can not vote")
